5c2498517d9a61beb0b02cd398e1bbcb0fd66613
kanon.che <kanon.che@ascendcorp.com>
2025-07-13 11:18:25 +0700

    Set my own name

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       {/* Navigation */}
       <nav className="navbar">
         <div className="nav-container">
-          <div className="nav-logo">KanonKC</div>
+          <div className="nav-logo">Kanon Cheewathasana</div>
           <ul className="nav-menu">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
